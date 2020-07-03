4813353cb125112d6faf778d15dc3f7c4d5e1e4c
rafaelalvesitm <rgomesal@hotmail.com>
2020-07-03 15:58:10 -0300

    Changed configuration on platform/Ageconf files

--- opcuatest/server.py
+++ opcuatest/server.py
@@ -3,36 +3,11 @@ sys.path.insert(0, "..")
 import time
 from opcua import ua, Server
 
-server = Server()
-
-server = Server()
-server.set_endpoint("opc.tcp://localhost:4840")
-
-name = "OPCUA_TEST"
-addspace = server.register_namespace(name)
-
-node = server.get_objects_node()
-
-Param = node.add_object(addspace , "CenterPivot")
-temp = Param.add_variable(addspace,"Temperature", 0)
-press = Param.add_variable(addspace,"Pressure", 0)
-time = Param.add_variable(addspace,"Time", 0)
-name = Param.add_variable(addspace, "name", "pivo central")
-
-
-temp.set_writable()
-press.set_writable()
-time.set_writable()
-
-server.start()
-print("server is starting!")
-
-
 if __name__ == "__main__":
 
     # setup our server
     server = Server()
-    server.set_endpoint("opc.tcp://0.0.0.0:4840/freeopcua/server/")
+    server.set_endpoint("opc.tcp://0.0.0.0:4841/freeopcua/server/")
 
     # setup our own namespace, not really necessary but should as spec
     uri = "rafael"
@@ -46,7 +21,7 @@ if __name__ == "__main__":
     myobj = objects.add_object(idx, "CenterPivot") # Add Center pivot object
 
     # Add variables for all sprinklers
-    sp1 = myobj.add_variable(idx, "sp1", 0)
+    sp1 = myobj.add_variable(idx, "sp1", 1 )
     sp1.set_writable() 
     sp2 = myobj.add_variable(idx, "sp2", 0)
     sp2.set_writable() 
@@ -73,13 +48,5 @@ if __name__ == "__main__":
 
     # starting!
     server.start()
-    
-    try:
-        count = 0
-        while True:
-            time.sleep(1)
-            count += 0.1
-            myvar.set_value(count)
-    finally:
-        #close connection, remove subcsriptions, etc
-        server.stop()
+    print(myobj)
+    print(time)
--- platform/OpcUAServer/server.py
+++ platform/OpcUAServer/server.py
@@ -11,14 +11,14 @@ addspace = server.register_namespace(name)
 
 node = server.get_objects_node()
 
-Param = node.add_object(addspace , "Parameters")
-temp = Param.add_variable(addspace,"Temperature", 0)
-press = Param.add_variable(addspace,"Pressure", 0)
-time = Param.add_variable(addspace,"Time", 0)
+Param = node.add_object('ns=3;i=1000' , "Parameters")
+temp = Param.add_variable('ns=3;s=Temperature',"Temperature", 0)
+press = Param.add_variable('ns=3;s=Pressure',"Pressure", 0)
+current = Param.add_variable('ns=3;s=Current',"Current", 0)
 
 temp.set_writable()
 press.set_writable()
-time.set_writable()
+current.set_writable()
 
 server.start()
 print("server is starting!")
