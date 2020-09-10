62e43cb4f1b5f4ace157cbbeb47bdb0398f4831f
rafaelalvesitm <rgomesal@hotmail.com>
2020-09-10 10:41:22 -0300

    Atualização nos containers para mostrar informações dos prints no comando docker logs

--- platform/probe/uploaddata.py
+++ platform/probe/uploaddata.py
@@ -3,26 +3,27 @@ from datetime import datetime
 import time
 import requests
 
-with open('SoilProbeData.csv') as csv_file:
+with open('sensores.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
     for row in csv_reader:
         if line_count == 0:
             line_count += 1
         else:
-            smr1 = row[3]
-            smr2 = row[5]
-            smc1 = row[4]
-            smc2 = row[6]
-            ah = row[1]
-            at = row[2]
-            st = row[7]
+            smr1 = row[4]
+            smr2 = row[6]
+            smc1 = row[5]
+            smc2 = row[7]
+            ah = row[2]
+            at = row[3]
+            st = row[8]
+            lum = row[1]
             print(smr1,smr2,smc1,smc2,ah,at,st)
 
             try:
                 url = f"http://iot-agent-json:7896/iot/json?i=Probe1&k=key123"
 
-                payload = f"{{\n    \"smr1\": {smr1},\n    \"smr2\":{smr2},\n    \"smc1\": {smc1},\n    \"smc2\": {smc2},\n    \"ah\": {ah},\n    \"at\": {at},\n    \"st\": {st}\n}}"
+                payload = f"{{\n \"smr1\": {smr1},\n \"smr2\":{smr2},\n \"smc1\": {smc1},\n \"smc2\": {smc2},\n \"ah\": {ah},\n \"at\": {at},\n \"st\": {st}\n,\n \"lum\": {lum}\n}}"
                 headers = {
                     'content-type': 'application/json',
                     'fiware-service': 'lab',
